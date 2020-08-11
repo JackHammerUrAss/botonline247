2418197d15f6b87fbcd753f19df6463b2e94ede6
jack` <dolfinroemer@gmail.com>
2020-08-11 17:10:48 +0200

    16

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ client.on('message', message =>{
  
 client.login(process.env.token);
 
+bot.on('message', message =>{
+    let args = message.content.substring(prefix.length).split(" ");
+    
+    switch(args[0]){
+        case "poll":
+            const Embed = new richEmbed()
+            .setColor(0xFFC300)
+            .setTitle("initiate Poll")
+            .setDescription("*poll to initiate a simple yes or no poll");
 
-
-
+            if(!args[1]){
+                message.channel.send(Embed);
+            }
+        break;
+            
+    }
+})
