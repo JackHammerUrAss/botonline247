7adf5f571135e5389196299f53485a5c919d8a22
jack` <dolfinroemer@gmail.com>
2020-08-11 19:55:14 +0200

    ewsda

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 const CommandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandfiles){
+for(const file of commandFiles){
     const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
