dd6c3924e0f4d391c55595a532dbad0ed799f296
jack` <dolfinroemer@gmail.com>
2020-08-12 00:12:18 +0200

    0

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+
 const Discord = require('discord.js');
 const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({disableEveryone: true});
-const ha = require 
-require("./util/eventHandler")
+
+require("./util/eventHandler")(bot)
 
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
+fs.readdir("./commands/", (err, files) => {
 
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
 
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
@@ -24,41 +41,42 @@ bot.on("message", async message => {
     if(commandfile) commandfile.run(bot,message,args)
 
 })
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
 
-bot.on("ready", async () => {
-    console.log(`${bot.user.username}is online`)
-    bot.user.setActivity("*help", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
+    let prefix = botsettings.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}hi`){
+        return message.channel.send("Hello")
+    }
+
+    if(cmd === `${prefix}clear`){
+        if (message.deletable) {
+            message.delete();
+        }
+
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("Missing Permissions!").then(m => m.delete(5000));
+        }
+
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("This is not a number").then(m => m.delete(5000));
+        }
+
+        let deleteAmount;
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount, true)
+        .catch(err => message.reply(`Something went wrong... ${err}`));
+    
+    }
+
 })
-
-
-module.exports.run = async (bot, message, args) => {
-            
-    if (message.deletable) {
-        message.delete();
-    }
-
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-        return message.reply("Missing Permissions!").then(m => m.delete(5000));
-    }
-
-    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
-        return message.reply("This is not a number").then(m => m.delete(5000));
-    }
-
-    let deleteAmount;
-    if (parseInt(args[0]) > 100) {
-        deleteAmount = 100;
-    } else {
-        deleteAmount = parseInt(args[0]);
-    }
-
-    message.channel.bulkDelete(deleteAmount, true)
-    .catch(err => message.reply(`Something went wrong... ${err}`));
-
-}
-
-module.exports.run = async (bot, message, args) => {
-    return message.channel.send("Hello")
-}
-
 bot.login(process.env.token);
