86c809be6e63b037fc9e6f685f360c4132b47b43
jack` <dolfinroemer@gmail.com>
2020-08-11 23:23:57 +0200

    test

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const botsettings = require('./botsettings.json');
 
 const bot = new Discord.Client({disableEveryone: true});
 
+require("./util/eventHandler")(bot)
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0) {
+        return console.log("[LOGS] Couldn't Find Commands!");    
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/$(f)`);
+        bot.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+
 bot.on("ready", async () => {
     console.log(`${bot.user.username}is online`)
     bot.user.setActivity("*help", {type: "PLAYING", url:"https://twitch.tv/Strandable"});
@@ -15,6 +40,11 @@ bot.on("message", async message => {
     let messageArray = message.content.split(" ")
     let cmd = messageArray[0];
     let args = messageArray.slice(1)
+
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+    
 
     if(cmd === `${prefix}hi`){
         return message.channel.send("Hello")
