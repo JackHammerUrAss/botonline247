124e4cdc60fd1bcdfcbb25b78659f4cf8280569b
jack` <dolfinroemer@gmail.com>
2020-08-12 00:01:47 +0200

    7

--- index.js
+++ index.js
@@ -9,23 +9,7 @@ const fs = require("fs");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
 
-    if(err) console.log(err)
-
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
-    if(jsfile.length <= 0) {
-         return console.log("[LOGS] Couldn't Find Commands!");
-    }
-
-    jsfile.forEach((f, i) => {
-        let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);  
-        pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
-        });
-    });
-});
 
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
@@ -46,5 +30,40 @@ bot.on("ready", async () => {
     bot.user.setActivity("*help", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
 })
 
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+
+
+module.exports.run = async (bot, message, args) => {
+            
+    if (message.deletable) {
+        message.delete();
+    }
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        return message.reply("Missing Permissions!").then(m => m.delete(5000));
+    }
+
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        return message.reply("This is not a number").then(m => m.delete(5000));
+    }
+
+    let deleteAmount;
+    if (parseInt(args[0]) > 100) {
+        deleteAmount = 100;
+    } else {
+        deleteAmount = parseInt(args[0]);
+    }
+
+    message.channel.bulkDelete(deleteAmount, true)
+    .catch(err => message.reply(`Something went wrong... ${err}`));
+
+}
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+
+module.exports.run = async (bot, message, args) => {
+    return message.channel.send("Hello")
+}
 
 bot.login(process.env.token);
