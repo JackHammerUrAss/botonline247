879aa35f51e4326c4b9ed03dff55c0ca7c173690
jack` <dolfinroemer@gmail.com>
2020-08-11 20:22:51 +0200

    fcew

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('message', message =>{
 
 
     } else if (command == 'user'){
-        client.commands.get('profile').execute(message, args);
+        client.commands.get('user').execute(message, args);
     
 
     } else if (command == 'youtube'){}
