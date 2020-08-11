ff333ac78bb3578c47f047b5aad96ba31027d1c4
jack` <dolfinroemer@gmail.com>
2020-08-11 17:33:57 +0200

    back yto 5

--- index.js
+++ index.js
@@ -32,20 +32,3 @@ client.on('message', message =>{
  
 client.login(process.env.token);
 
-bot.on('message', message =>{
-    let args = message.content.substring(prefix.length).split(" ");
-    
-    switch(args[0]){
-        case "poll":
-            const Embed = new MessageEmbed()
-            .setColor(0xFFC300)
-            .setTitle("initiate Poll")
-            .setDescription("*poll to initiate a simple yes or no poll");
-
-            if(!args[1]){
-                message.channel.send(Embed);
-            }
-        break;
-            
-    }
-})
