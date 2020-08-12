fd4d614bdbe36f4c8e21efddefad91bea2f6e827
jack` <dolfinroemer@gmail.com>
2020-08-12 12:00:56 +0200

    edc

--- commands/embed.js
+++ commands/embed.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
     const embed = new Discord.MessageEmbed()
      .setTitle('This is a test Embed.')
+     .setURL('https://www.roblox.com/users/92044277/profile')
     
     message.channel.send(embed)
 }
