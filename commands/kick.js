7f7ff55e27ffcbdf8536a9e79afe67f53727d9c6
jack` <dolfinroemer@gmail.com>
2020-08-12 13:08:32 +0200

    d

---
+++ commands/kick.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission('KICK_MEMBERS'))
+        message.channel.send("you don't have permission to use that command!");
+    else {
+        let member = message.guild.member.cache.get(args);
+        if(member) {
+        try {
+            await member.kick();
+            console.log('kicked');
+            message.channel.send(`${member}, kicked!`)
+        }
+            catch(err) {
+            console.log(err) ;         
+        }
+    }
+}
+}
+
+module.exports.config = {
+    name: "kick",
+    Description: "kicks a user",
+    usage: "?kick", 
+    accessableby: "Admins",
+    aliases: []
+}
