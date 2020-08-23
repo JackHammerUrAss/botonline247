3b328b45a92db2f9bcb1f1fffbae2d6f9df00886
jack` <dolfinroemer@gmail.com>
2020-08-24 00:24:13 +0200

    d

---
+++ events/ready.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client();
+
+module.exports = bot => { 
+    console.log(`${bot.user.username} is online`)
+    function randomStatus() {
+        let status = ["Apocolyptic Rust", "a!help", `over ${bot.users.cache.size} users!`, `over ${bot.guilds.cache.size} servers!`]
+        let rstatus = Math.floor(Math.random() * status.length);
+        bot.user.setActivity(status[rstatus], {type: "STREAMING", url: 'https://www.twitch.tv/grandmasterbot_twitch'});
+    }; setInterval(randomStatus, 10000)
+
+  
+}
