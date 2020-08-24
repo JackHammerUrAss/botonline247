0ea201d425466044b323eeba81c072e37a98aeea
jack` <dolfinroemer@gmail.com>
2020-08-24 22:34:27 +0200

    e

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ bot.on("message", async message => {
     if(commandfile) commandfile.run(bot,message,args)
     if(cmd === `${prefix}tj`){
         let embed = new Discord.MessageEmbed()
-        .setTitle('**WELCOME TO APOCOLYPTIC KNIGHTS**')
+        .setTitle('**WELCOME TO APOCOLYPTIC KNIGHTS**', 'https://lh3.googleusercontent.com/9MqtYaV8CA1YmuZ4xo05hoMrbzskV9tsjc7HWA7F5sx-gjeA1Dm56fDqTHPH--j5DQ-OpA=s85')
         .setURL('https://dolfinroemer.wixsite.com/apocolyptic-knights')
         .setAuthor('Welcome!', 'https://cdn3.iconfinder.com/data/icons/flat-actions-icons-9/792/Tick_Mark_Dark-512.png')
         .setColor('FFE8E8')
