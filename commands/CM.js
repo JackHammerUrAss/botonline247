5744689cb108ca038df3a5ba526b5e1791ffe884
jack` <dolfinroemer@gmail.com>
2020-08-25 17:13:22 +0200

    e

--- commands/CM.js
+++ commands/CM.js
@@ -4,18 +4,13 @@ const botconfig = require("../botsettings.json");
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission('MANAGE_CHANNELS'))
         message.channel.send("You don't have permission to use that command.");
-    else {
-        let member = message.guild.members.cache.get(args);
-        if(member) {
-        try {
-            message.channel.send(`${member}, coming`)
-        }
-            catch(err) {
-            console.log(err);
-        }
+     else {
+        message.reply('Bot is Online!')
+        
+         
     }
 }
-}
+
 
 module.exports.config = {
     name: "Creative Manager",
