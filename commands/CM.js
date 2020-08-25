e780afc520236790ba5170950c9d5e8909d42ba1
jack` <dolfinroemer@gmail.com>
2020-08-25 18:51:23 +0200

    D

--- commands/CM.js
+++ commands/CM.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
         .setURL('https://dolfinroemer.wixsite.com/apocolyptic-knights')
         .setAuthor("www.ApocolypticKnights.com")
         .setColor('FFE8E8')
-        .setThumbnail('https://lh3.googleusercontent.com/9MqtYaV8CA1YmuZ4xo05hoMrbzskV9tsjc7HWA7F5sx-gjeA1Dm56fDqTHPH--j5DQ-OpA=s85')
+        .setThumbnail('https://lh3.googleusercontent.com/xP0pfWaYe_6pXGp35gL37_pbSsjfNZLYlCi6fpSlxoFZ8B9yaMb_Y5CgmfZLyeUsv9E4=s85')
         .setFooter('ApkBot', 'https://lh3.googleusercontent.com/9MqtYaV8CA1YmuZ4xo05hoMrbzskV9tsjc7HWA7F5sx-gjeA1Dm56fDqTHPH--j5DQ-OpA=s85')
         .addFields(
             { name: '**You are a Creative Manager?**', value: 'and you dont exacly know what to do or expect? \n we will help you a bit on that: \n you can click the blue line above to go the webpage with a very clear explanation on what to do.', inline: true},
