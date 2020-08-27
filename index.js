6129b71944d3a72b4bc32a6effad6233144ce028
jack` <dolfinroemer@gmail.com>
2020-08-28 00:23:02 +0200

    d

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ bot.on("messageReactionAdd", async (reaction, user) => {
     if(reaction.message.channel.id === "743069846685614141"){
       if (reaction.emoji.name === '✅'){
           await reaction.message.guild.members.cache.get(user.id).roles.add('743589337945604196')
-          const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
+          const welcomeChannel = reaction.message.guild.channels.cache.find(channel => channel.name === 'welcome')
           reaction.message.welcomeChannel.send(` user: ${user} has verified `)
       }  
     }
