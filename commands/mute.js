07277976f67a7e9503da3d3e57ee619df22123e1
jack` <dolfinroemer@gmail.com>
2020-08-28 00:42:59 +0200

    e

--- commands/mute.js
+++ commands/mute.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 
-odule.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission(['MUTE_MEMBERS'])) return;
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
     if(member.hasPermission(['MUTE_MEMBERS']) && !message.member.hasPermission('MUTE_MEMBERS')) return;
