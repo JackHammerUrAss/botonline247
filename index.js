34696c9085dee417ed6895cf96824988023540c7
jack` <dolfinroemer@gmail.com>
2020-09-02 00:05:42 +0200

    e

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const botsettings = require('./botsettings.json');
 const bot = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-bot.botsettings = botsettings; 
 
 
 bot.on("guildMemberAdd", member => {
