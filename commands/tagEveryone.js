90167c105c84d67f11ae364720280ea817792f03
jack` <dolfinroemer@gmail.com>
2020-09-02 22:19:05 +0200

    e

--- commands/tagEveryone.js
+++ commands/tagEveryone.js
@@ -1,25 +1,31 @@
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
+const usedCommand = new Set()
+
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('ADMINISTARTOR'))
-        message.channel.send("You don't have permission to use that command.");
+    if(!message.member.hasPermission('ADMINISTRATOR'))
+        message.channel.send('you dont have permission to use this command');
      else {
-       message.Channel.channel.send('@everyone')
-            
-        
-       
-     
-        
-         
+        message.reply('@everyone')
+
     }
-}
+
+    
+        
+        
+        usedCommand.add(message.author.id);
+        setTimeout(() => {
+            usedCommand.delete(message.author.id);
+        }, 5000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+    }
+
 
 
 module.exports.config = {
-    name: "tag everyone",
-    description: "sends a link",
-    usage: "a!te",
-    accessableby: "Creative Managers",
-    aliases: ['tagAll', 'ta']
+    name: "tagEveryone",
+    description: "checks if the bot is online",
+    usage: "a!tagEveryone",
+    accessableby: "Admins",
+    aliases: ['tagEveryone', 'te']
 }
