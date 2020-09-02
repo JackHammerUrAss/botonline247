6c5c1125ed920ec5946657cb2702ff4dcec5744e
jack` <dolfinroemer@gmail.com>
2020-09-02 22:50:02 +0200

    we

--- commands/tagEveryone.js
+++ commands/tagEveryone.js
@@ -1,13 +1,12 @@
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
-const everyoneChannel = message.mentions.channels.first();
 const usedCommand = new Set()
 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission('ADMINISTRATOR'))
         message.channel.send('you dont have permission to use this command');
      else {
-        if(!everyoneChannel) return message.channel.send('Please provide a channel');
+        const everyoneChannel = message.mentions.channels.first();
         everyoneChannel.send('@everyone')
 
     }
@@ -18,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
         usedCommand.add(message.author.id);
         setTimeout(() => {
             usedCommand.delete(message.author.id);
-        }, 5000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
+        }, 5000);
     }
 
 
