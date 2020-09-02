1a7b5418a5bcafa04ef8f7ba9a3e7c45e63a799e
jack` <dolfinroemer@gmail.com>
2020-09-02 23:17:56 +0200

    w

--- commands/tagEveryone.js
+++ commands/tagEveryone.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
      else {
         const everyoneChannel = message.mentions.channels.first();
         everyoneChannel.send('@everyone')
-        const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'moderation')
+        const welcomeChannel = message.guild.channels.cache.find(channel => channel.name === 'moderation')
         welcomeChannel.send(`user: ${user} has tagges everyone in ${everyoneChannel}`)
 
     }
