f0a09c882ee8a998aeef567e531d081e4bc86351
jack` <dolfinroemer@gmail.com>
2020-09-06 23:39:13 +0200

    w

--- commands/online.js
+++ commands/online.js
@@ -1,20 +1,29 @@
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
+
 const usedCommand = new Set()
 
 module.exports.run = async (bot, message, args) => {
     if(usedCommand.has(message.author.id)){
         message.reply('You cannot use the command beacuse of the cooldown.')
     } else {
-        message.reply('Bot is Online!')
+        const embed = new Discord.MessageEmbed()
+        .setTitle('The Bot is online!')
+        .setFooter('TA', bot.user.displayAvatarURL())
+        .setTimestamp()
+        
+        message.channel.send(embed);
+     
+    }
+}
       
         
         usedCommand.add(message.author.id);
         setTimeout(() => {
             usedCommand.delete(message.author.id);
         }, 5000); //You can set the ammount of the cooldown here! Its Formated to Miliseconds.
-    }
-}
+    
+
 
 
 module.exports.config = {
