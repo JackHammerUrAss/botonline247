bdbc4e41c1335d87fcf57814375fe3391fb6ca15
jack` <dolfinroemer@gmail.com>
2020-09-10 17:07:49 +0200

    e

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,11 +2,11 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission('ADMINISTRATOR'))
     message.channel.send('you dont have permission to use this command');
  else {
-    let epicRole = message.guild.roles.cache.get('743589337945604196');
+    let epicRole = message.guild.roles.cache.get('748914648673157200');
     const member = message.mentions.members.first();
 
     member.roles.add(epicRole);
-    message.channel.send('Role Added')
+    message.channel.send("Role 'team' Added")
 }}
 
 module.exports.config = {
