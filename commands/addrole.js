e152328c42cb57746bedc8d38461ec89ca1cfc5e
jack` <dolfinroemer@gmail.com>
2020-09-11 00:13:35 +0200

    w

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,12 +1,28 @@
+const { DiscordAPIError } = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('ADMINISTRATOR'))
+    if(!message.member.hasPermission('ADMINISTRATOR', 'MANAGE_ROLES'))
     message.channel.send('you dont have permission to use this command');
  else {
     let epicRole = message.guild.roles.cache.get('748914648673157200');
+    if (!epicRole) return message.channel.send("Please provide a user to add a role to.")
+    let role = message.guild.roles.find(r => r.name == args[1]) || message.guild.roles.find(r => r.id == args[1]) || message.mentions.roles.first()
+    if(!role) return message.channel.send("Please provide a role to add to said user.")
+    let reason = args.slice(2).join(" ")
+    if(!reason) return message.channel.send("Please provide a reason/argument")
+    
+    if(!message.guild.me.hasPermission('ADMINISTRATOR', 'MANAGE_ROLES')) return  message.channel.send('you dont have permission to perform');
+
+    if(epicRole.roles.has(role.id)) {
+        return message.channel.send(`${epicRole.displayName}, already has provided role`)
+    } else {
+        await epicRole.addrole(role.id).catch(e => console.log(e.message))
+        message.channel.send(`The role, ${role.name}, has been added to ${epicRole.displayName}.`)
+    }
+
     const member = message.mentions.members.first();
 
-    member.roles.add(epicRole);
-    message.channel.send("Role 'team' Added")
+
 }}
 
 module.exports.config = {
@@ -14,6 +30,6 @@ module.exports.config = {
     description: "adds member role(default;member), to use this command simply type ```addrole'availabe role here'```, the availabe roles are linked in the pinged message in the staff channel",
     usage: "z!addrole",
     accessableby: "Members",
-    aliases: []
+    aliases: ["ar"]
 }
 
