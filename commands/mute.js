1a21455dc8ba43fd80f1cd0b66f3ffa566c797c8
jack` <dolfinroemer@gmail.com>
2020-09-12 23:18:47 +0200

    e

--- commands/mute.js
+++ commands/mute.js
@@ -5,7 +5,7 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission(['MUTE_MEMBERS'])) return;
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
+    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0])
     if(member.hasPermission(['MUTE_MEMBERS']) && !message.member.hasPermission('MUTE_MEMBERS')) return;
 
         let mutedRole = message.guild.roles.cache.get('748304191738609804');
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
             message.channel.send("User was Successfully muted.");
             const welcomeChannel = message.guild.channels.cache.find(channel => channel.id === '743829943221354506')
             if (!welcomeChannel) return;
-            welcomeChannel.send(` someone has been muted `)
+            welcomeChannel.send(` ${member} has been muted `)
         }
 }
 
