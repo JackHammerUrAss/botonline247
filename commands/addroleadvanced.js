97ea6a4d8af038acc22ea286be0d7bff1cbe6c76
jack` <dolfinroemer@gmail.com>
2020-09-13 23:28:42 +0200

    w

--- commands/addroleadvanced.js
+++ commands/addroleadvanced.js
@@ -3,15 +3,15 @@ module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You dont have permission to use this command /:');
     const user = message.mentions.members.first();
     if(!user) return message.channel.send('Please specify a user you would like to give a role to')
-    const role = member.guild.roles.cache.find(r=> r.name === args.slice(1).join(" "));
+    const role = guild.roles.cache.find(r => r.name === 'role name');
     if(!role) return message.channel.send('Please specify a role you like to give to the Mentioned user')
-    await user.role.add(role), message.channel.send(`${user} now has the ${role} role`)
+    await user.roles.add(role), message.channel.send(`${user} now has the ${role} role`)
 }
 
 module.exports.config = {
     name: "addrole",
     description: "adds member role(default;member), to use this command simply type ```addrole'availabe role here'```, the availabe roles are linked in the pinged message in the staff channel",
-    usage: "z!addrole",
+    usage: "a!addrole",
     accessableby: "Members",
     aliases: ["ar"]
 }
