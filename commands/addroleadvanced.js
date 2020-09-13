6a680bf465a31e792eae46fb75fe8667f33845ca
jack` <dolfinroemer@gmail.com>
2020-09-13 23:55:54 +0200

    e

--- commands/addroleadvanced.js
+++ commands/addroleadvanced.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send('You dont have permission to use this command /:');
     const user = message.mentions.members.first();
     if(!user) return message.channel.send('Please specify a user you would like to give a role to')
-    const role = message.guild.roles.find(r => r.name === 'Role name');
+    const role = message.guild.roles.cache.find(r => r.name === 'Role name');
     if(!role) return message.channel.send('Please specify a role you like to give to the Mentioned user')
      user.roles.add(role);
     message.channel.send(`${user} now has the ${role} role`)
