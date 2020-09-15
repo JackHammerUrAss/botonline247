7baa7b039ee9a317f36c02191031c5e3526ba55e
jack` <dolfinroemer@gmail.com>
2020-09-15 17:23:56 +0200

    e

--- commands/addroleadvanced.js
+++ commands/addroleadvanced.js
@@ -5,7 +5,6 @@ module.exports.run = async (client, message, args) => {
     if(!user) return message.channel.send('Please specify a user you would like to give a role to')
     const role = message.guild.roles.cache.find(r => r.name === 'Role name');
     if(!role) return message.channel.send('Please specify a role you like to give to the Mentioned user')
-    console.log()
      user.roles.add(role);
     message.channel.send(`${user} now has the ${role} role`)
 }
