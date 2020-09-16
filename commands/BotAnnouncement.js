346da7d1779e5d791d1c04ff91bd7092e24350df
jack` <dolfinroemer@gmail.com>
2020-09-16 22:55:37 +0200

    w

--- commands/BotAnnouncement.js
+++ commands/BotAnnouncement.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
     
     const messageArray = message.content.split(' ');
     const cmd = messageArray[0];
-    const args1 = messageArray.slice(1);
+
 
     if (cmd === '!ba'){
         let pollChannel = message.mentions.channels.first();
-        let pollDescription = message.content.substring(message.content.indexOf(' ')+1);
+        let pollDescription = args.slice(1).join(' ');
 
         let embedPoll = new Discord.MessageEmbed()
         .setTitle('New bot update!')
