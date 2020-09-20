1c306dd4a41294743de400c8802acf3530393158
jack` <dolfinroemer@gmail.com>
2020-09-20 13:58:05 +0200

    w

--- commands/ban.js
+++ commands/ban.js
@@ -4,15 +4,19 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
 
+    if (message.deletable) {
+        message.delete();
+    }
+
     let messageArray = message.content.split(" ");
     let args2 = messageArray.slice(1)
 
     if(!message.member.hasPermission('BAN_MEMBERS')) 
-        message.channel.send("You don't have permission to use that command.");
+        message.channel.send("You don't have permission to use that command.").then(m => m.delete({ timeout: 5000}));
     else {
         let member = message.guild.members.cache.get(args[0]) || message.guild.member(message.mentions.members.first()); 
         if(!member){
-            return message.reply('Please state a user or userID')}
+            return message.reply('Please state a user or userID').then(m => m.delete({ timeout: 5000}))};
         let bReason = args2.join(" ").slice(22)
         
         if(member){
@@ -20,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
         try {
             await member.ban();
             console.log(member.tag + " was banned.");
-            message.channel.send (`${member} Have been Banned from The Server!`)
+            message.channel.send (`${member} Have been Banned from The Server!`).then(m => m.delete({ timeout: 5000}));
 
             let banEmbed = new Discord.MessageEmbed()
             .setDescription("ban")
@@ -33,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
             .setTimestamp();
 
             let banChannel = member.guild.channels.cache.find(channel => channel.id === '749752743710687282')
-            if(!banChannel) return message.channel.send("Can't find incidents channel.")
+            if(!banChannel) return message.channel.send("Can't find incidents channel.").then(m => m.delete({ timeout: 5000}));
 
             banChannel.send(banEmbed)
         }
--- commands/kick.js
+++ commands/kick.js
@@ -1,29 +1,61 @@
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
+
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('KICK_MEMBERS'))
-        message.channel.send("You don't have permission to use that command.");
+
+    if (message.deletable) {
+        message.delete();
+    }
+
+    let messageArray = message.content.split(" ");
+    let args2 = messageArray.slice(1)
+
+    if(!message.member.hasPermission('KICK_MEMBERS')) 
+        message.channel.send("You don't have permission to use that command.").then(m => m.delete({ timeout: 5000}));
     else {
         let member = message.guild.members.cache.get(args[0]) || message.guild.member(message.mentions.members.first()); 
-        if(member) {
+        if(!member){
+            return message.reply('Please state a user or userID').then(m => m.delete({ timeout: 5000}))};
+        let kReason = args2.join(" ").slice(22)
+        if(!kReason){
+            return message.reply('Please provide a reason.').then(m => m.delete({ timeout: 5000}));
+        }
+        }
+        
+        if(member){
+
         try {
-            await member.kick();
-            console.log('Kicked');
-            const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'moderation')
-            message.welcomeChannel.send(`${member}, Kicked!`)
+            await member.ban();
+            console.log(member.tag + " was Kicked.");
+            message.channel.send (`${member} Have been Kicked from The Server!`).then(m => m.delete({ timeout: 5000}));
+
+            let kickEmbed = new Discord.MessageEmbed()
+            .setDescription("***Kick***")
+            .setColor('#e56b00')
+            .addField("Banned User", `${member} with ID ${member.id}`)
+            .addField("Banned by", `<@${message.author.id}> with ID ${message.author.id}`)
+            .addField("Banned in", message.channel)
+            .addField("time", message.createdAt)
+            .addField("reason", kReason)
+            .setTimestamp();
+
+            let kickChannel = member.guild.channels.cache.find(channel => channel.id === '749752743710687282')
+            if(!kickChannel) return message.channel.send("Can't find incidents channel.").then(m => m.delete({ timeout: 5000}));
+
+            kickChannel.send(kickEmbed)
         }
             catch(err) {
             console.log(err);
         }
     }
 }
-}
+
 
 module.exports.config = {
-    name: "kick",
-    description: "Kicks a user",
-    usage: "*kick",
+    name: "ban",
+    description: "Bans a Users",
+    usage: "*ban",
     accessableby: "Admins",
     aliases: []
 }
