fd1f90ffad85eeff8de97e6341f914b56d6ebbac
jack` <dolfinroemer@gmail.com>
2020-09-22 23:44:48 +0200

    f

--- events/ready.js
+++ events/ready.js
@@ -12,7 +12,7 @@ module.exports = bot => {
     })
 
     function randomStatus() {
-        let status = ["Apocolyptic Rust", "a!help", ` over ${bot.users.cache.size} users!`, ` over ${bot.guilds.cache.size} servers!`]
+        let status = ["Life Support Systems || !help", "!help for help || !help", ` over ${bot.users.cache.size} users! || !help`, ` over ${bot.guilds.cache.size} servers! || !help`,' over general conversation || !help']
         let rstatus = Math.floor(Math.random() * status.length);
         bot.user.setActivity(status[rstatus], {type: "WATCHING"});
     }; setInterval(randomStatus, 10000)
